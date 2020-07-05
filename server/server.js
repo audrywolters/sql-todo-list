35b51c5e3cc0d0ae50cdb6fa8f20a7c3694a9145
Audry Wolters <audrywolters@gmail.com>
2020-07-04 23:28:09 -0500

    server running

---
+++ server/server.js
@@ -0,0 +1,15 @@
+// require
+const express = require( 'express' );
+const app = express();
+const bodyParser = require( 'body-parser' );
+//const todos = require( './modules/todos.route' );
+
+// use
+app.use( bodyParser.urlencoded( { extended: true } ) );
+app.use( express.static( 'server/public' ) );
+
+// go server!
+const port = 5000;
+app.listen( port, () => {
+    console.log( 'server running!: ' + port );
+})
