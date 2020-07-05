29755e6a3fa618a9b27bb6bb773399d2535ea0f5
Audry Wolters <audrywolters@gmail.com>
2020-07-05 01:13:05 -0500

    printing data in table

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -7,13 +7,36 @@ function onReady() {
     getTodos();
 }
 
+// let todos = [];
+
 // GET all
 function getTodos() {
     $.ajax({
         type: 'GET',
         url: '/todos'
     }).then( function( response ){
-        console.log( 'sucessed GET: ' + response );
+        // find the element in which to display the todos
+        let tableBody = $( '#todosDisplay' );
+
+        // we're going to have to wipe everything out
+        tableBody.empty();
+
+        // AUDRY - why did he do this?
+        // hold response in a global
+        //todos = response;
+ 
+        // we're going to have to reprint everything each time
+        // AUDRY - how the f are we going to check a checkbox by bool value?
+        // css pseudo - can i do that?
+        for ( let row of response ) {
+            tableBody.append( 
+                `<tr>
+                    <td>${ row.task }</td>
+                    <td>${ row.completed }</td>
+                    <button>Delete</button>
+                 </tr>`
+            )    
+        }
     }).catch( function( err ) {
         console.log( 'GET failed: ' + err );
     })
